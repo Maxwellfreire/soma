2f5f6ee99301c4fefec5315bd0dd803bdd10b77f
Maxwellfreire <maxwell201414@gmail.com>
2019-02-04 20:11:47 -0200

    correção de bugs

--- src/Matematica/Problema.java
+++ src/Matematica/Problema.java
@@ -4,6 +4,8 @@ public class Problema {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Maxwell teste");
+		System.out.println("Freire");
 
 	}
 
