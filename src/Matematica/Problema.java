2dbef9ebdd432dae8527c2f0ece1d55f018318fc
Maxwellfreire <maxwell201414@gmail.com>
2019-02-04 20:20:55 -0200

    versão_2019

--- src/Matematica/Problema.java
+++ src/Matematica/Problema.java
@@ -4,8 +4,18 @@ public class Problema {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int x,y , soma;
+		
+		
 		System.out.println("Maxwell teste");
 		System.out.println("Freire");
+		
+		x=2;
+		y=3;
+		double resultado = x+y;
+		
+		System.out.println(resultado);
+		
 
 	}
 
